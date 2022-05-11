c9086b06f175def79e48b020a5605a6f3d81312b
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 11:46:46 +0300

    Update Ambassador.hpp

--- sources/Ambassador.hpp
+++ sources/Ambassador.hpp
@@ -1,22 +1,28 @@
 #pragma once
 
 #include "Player.hpp"
-#include "Game.hpp"
+//#include "Game.hpp"
 
 //#ifndef GAME_HPP
 //#define GAME_HPP
-#include "Captain.hpp"
+//#include "Captain.hpp"
+#include "Game.hpp"
 //#endif
 namespace coup {
+    class Player;
+//    class Player;
     class Ambassador : public coup::Player {
     private:
-        Game game;
-        string _name;
+//        Game game;
+        string _role;
+//        Player p;
+//        string _name;
     public:
-        Ambassador(Game g,  string str);
+        Ambassador(Game &g,  string str);
 
         void block(const Player &p);
-        void transfer(const Player &p1, const Player &p2);
+        void transfer( Player &p1,  Player &p2);
+        string role();
 
         void unsteal();
 
