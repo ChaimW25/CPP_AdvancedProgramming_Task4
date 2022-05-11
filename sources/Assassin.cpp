3020bddf75ff4113ec0341905c7884643a784319
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 11:47:08 +0300

    Update Assassin.cpp

--- sources/Assassin.cpp
+++ sources/Assassin.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Player.hpp"
+//#include "Player.hpp"
 #include "Assassin.hpp"
 #include <cstring>
 using namespace std;
@@ -7,13 +7,44 @@ using namespace std;
 namespace coup {
 
 
-Assassin::Assassin(Game g, string name){
-    this->game=move(g);
-    this->_name=name;
+Assassin::Assassin(Game &g, string name): Player(g,name){
+//    this->game=move(g);
+//    this->_name=name;
     this->_role="Assassin";
-    this->coinNum=0;
-    g.addPlayer(*this);
+//    this->coinNum=0;
 }
+
+    string Assassin::role() {
+    return "Assassin";
+}
+    void Assassin::coup( Player &p) {
+        if(validTurn()){
+            if (coinNum<3){
+                throw("not enough coins");
+            }
+            else {
+                if(coinNum<7){
+                    game.removePlayer(p);
+                    this->coinNum-=3;
+                    game.turnIncrement();
+                }
+                else{
+                    game.removePlayer(p);
+                    this->coinNum-=7;
+                    game.turnIncrement();
+                    cout<<"nows turn??"<<this->game.turn()<<endl;
+
+                }
+            }
+
+//            this->game.GameTurn++;
+        }
+        else{
+            throw("Illegal turn");
+        }
+
+}
+
 
 }
 
