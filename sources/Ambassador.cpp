1096861237e07268f285461b4bee8bf11ff06ec5
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 19:33:28 +0300

    Update Ambassador.cpp

--- sources/Ambassador.cpp
+++ sources/Ambassador.cpp
@@ -7,39 +7,63 @@ using namespace std;
 namespace coup {
 
     Ambassador::Ambassador(Game &g,  string name): Player(g,name) {
-//        this->game = std::move(g);
-//        this->_name = name;
         this->_role="Ambassador";
-//        this->coinNum=0;
-//        this->game.size++;
-//        cout<<"hiii";
     }
 
-    void Ambassador::block(const Player &p){
-        this->game.turnIncrement();
-
+    void Ambassador::block( Player &p){
+        if(coinNum>=10){
+            throw("have to coup");
+        }
+        if (legalAmount() ) {
+            if(p.lastAction==2){
+                int temp=p.enemy->coinsIStoled;
+                p.coinNum-=temp;
+                p.enemy->coinNum+=temp;
+                p.lastAction=-1;
+            }
+        }
+        else {
+            throw ("Illegal turn or amount");
+        }
     }
 
     void Ambassador::transfer( Player &p1,  Player &p2){
-        if(p1.coinNum>=1) {
-            p1.coinNum -= 1;
-            p2.coinNum += 1;
-            this->game.turnIncrement();
-            cout<<"nows turn??"<<this->game.turn()<<endl;
+        if(coinNum>=10){
+            throw("have to coup");
         }
-        else{
-            throw("not enough money");
+        if (legalAmount() && validTurn()) {
+            if(p1.coinNum>=1) {
+                p1.coinNum -= 1;
+                p2.coinNum += 1;
+                this->game.nextTurn();
+                lastAction=2;
+            }
+            else{
+                throw("not enough money");
+            }
         }
-
+        else {
+                throw ("Illegal turn or amount");
+            }
     }
-    void Ambassador::unsteal(){
-        this->game.turnIncrement();
 
-
-    }
     string Ambassador::role(){
         return "Ambassador";
     }
+//    void Ambassador::unsteal(){
+//        if(coinNum>=10){
+//            throw("have to coup");
+//        }
+//        if (legalAmount() && validTurn()) {
+//            this->game.nextTurn();
+//        }
+//        else {
+//            throw ("Illegal turn or amount");
+//        }
+//    }
+
+
 
 }
+
 
