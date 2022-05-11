ec365a3fe4b23d4d46085ad46257d5a2cca7df55
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 19:37:00 +0300

    Update Duke.cpp

--- sources/Duke.cpp
+++ sources/Duke.cpp
@@ -7,27 +7,42 @@ using namespace std;
 namespace coup {
 
     Duke::Duke(Game &g,  string name) : Player(g,name){
-//        this->game = move(g);
-//        this->_name = n;
         this->_role="Duke";
-//        this->coinNum=0;
     }
-    void Duke::block(const Player &p) {
-        this->game.turnIncrement();
+
+    void Duke::block( Player &p) {
+        if (legalAmount() ) {
+            if(p.lastAction==1){
+                p.coinNum-=2;
+                p.lastAction=-1;
+            }
+            else {
+                throw ("Illegal turn");
+            }
+        }
+        else {
+            throw ("Illegal turn");
+        }
 
     }
+
     void Duke::tax() {
-        this->coinNum+=3;
-        cout<<"the turn is"<<this->getName()<<endl;
-        this->game.turnIncrement();
-        cout<<"nows turn??"<<this->game.turn()<<endl;
-
-
+        if(coinNum>=10){
+            throw("have to coup");
+        }
+        if (legalAmount() && validTurn()) {
+            this->coinNum+=3;
+            lastAction=-1;
+            game.nextTurn();
+        }
+        else {
+                throw ("Illegal turn");
+        }
     }
+
     string Duke::role() {
         return "Duke";
     }
 
 }
-
 
