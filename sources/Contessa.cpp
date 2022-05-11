c427521ede5126df7032a41df76912baa44ed963
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 19:36:16 +0300

    Update Contessa.cpp

--- sources/Contessa.cpp
+++ sources/Contessa.cpp
@@ -7,23 +7,59 @@ using namespace std;
 namespace coup {
 
     Contessa::Contessa(Game &g, string name): Player(g,name) {
-//    this->game = move(g);
-//    this->_name = n;
     this->_role="Contessa";
-//    this->coinNum=0;
+    }
 
+    void Contessa::block( Player &p) {
+//        cout << legalAmount()<<validTurn()<<endl;
+        if (legalAmount()) {
+            if (p.lastAction == 3) {
+                p.enemy->alive = true;
+                p.lastAction = -1;
+            }
+            else{
+                throw("no coup relevant");
+            }
+        }
+        else {
+            throw ("Illegal amount");
+        }
     }
 
 
 
-    void Contessa::block(const Player &p){
-        this->game.turnIncrement();
 
-    }
+
+
+
+
+
+//                uint place = 0;
+//            for (Player *p: this->game.playerNamesP) {
+//                if (!p->alive) {
+//                    if (this->game.playerNamesP.at(place) == p) {
+//                        this->game.playerNamesP.at(place)->alive = true;
+//                        game.nextTurn();
+//                        break;
+//                    }
+//                    place++;
+//                }
+//            }
+//        }
+//        else {
+//            throw ("Illegal turn");
+//        }
+//    }
+//    else{
+//        throw ("Illegal amount");
+//    }
+//}
+//        this->play = "block";
+//        this->game.turnIncrement();
+
+
     string Contessa::role(){
         return "Contessa";
     }
 }
-
-
 
