c7ef85bd96bc3e3ac1ce860cd18ae7612e4d983d
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 11:47:51 +0300

    Update Captain.cpp

--- sources/Captain.cpp
+++ sources/Captain.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
-#include "Player.hpp"
+//#include "Player.hpp"
 #include "Captain.hpp"
 #include <cstring>
 using namespace std;
 
 namespace coup {
 
-    Captain::Captain(Game g, string n) {
-        this->game = move(g);
-        this->_name = n;
+    Captain::Captain(Game &g, string name): Player(g,name) {
+//        this->game = move(g);
+//        this->_name = n;
         this->_role="Captain";
-        this->coinNum=0;
-        g.addPlayer(*this);
+//        this->coinNum=0;
     }
-    void Captain::steal(const Player &p){
+    void Captain::steal( Player &p){
+        if(validTurn()){
+            if (p.coinNum>=2){
+                p.coinNum-=2;
+                this->coinNum+=2;
+                game.turnIncrement();
+            }
+        else {
+            this->coinNum+=p.coinNum;
+            p.coinNum=0;
+            game.turnIncrement();
+
+            }
+
+//            this->game.GameTurn++;
+        }
+        else{
+            throw("Illegal turn");
+        }
+
     }
     void Captain::block(const Player &p){
+        this->game.turnIncrement();
+
 
     }
-
+    string Captain::role(){
+        return "Captain";
+    }
 }
 
