19180b5152b9a294a33c9825a3f801f0a7f1f6f6
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 19:37:44 +0300

    Update Game.cpp

--- sources/Game.cpp
+++ sources/Game.cpp
@@ -9,130 +9,91 @@ using namespace std;
 namespace coup {
 
     Game::Game() {
-//        this->size=0;
-        this->GameTurn=0;
-//        this->playerNamesP ;
-//        this->playerNames;
 
     }
-      vector<string> Game::players() {
-////        vector<string> s;
+
+    vector<string> Game::players() {
           vector<string> v ;
-//          return v;}
           string str;
           for(size_t i=0; i < this->playerNamesP.size(); i++){
-              str=this->playerNamesP.at(i)->getName();
-              v.push_back(str);
-//              cout << name << endl;
-//              cout<<*name<<endl;
-//              this->playerNames =(*)this->playerNamesP;
+              if (this->playerNamesP.at(i % this->playerNamesP.size())->alive) {
+                  str = this->playerNamesP.at(i)->getName();
+                  v.push_back(str);
+              }
           }
-        cout<<v.size()<<endl;
-//        cout<<v.at(1)<<endl;
         return v;
     }
-//    vector<Player> Game::getPlayers(){
-////        vector<string*>vec=this->playerNames;
-//        vector<string> vec=this->playerNames;
-//        vector<int*> i;
-////        vec*  i;
-//        return i;
+
+//    vector<Player*>* Game::myVector(){
+//        return &this->playerNamesP;
 //    }
-    vector<Player*>* Game::myVector(){
-        return &this->playerNamesP;
-    }
-    void Game::addPlayer(Player *name){
-//        vector<Player*>* v = this->playerNamesP;
-        vector<Player*>* vec=myVector();
-        cout<<"till now ok"<<endl;
-        cout<<vec->size()<<endl;
-        vec->push_back(name);
-        cout<<"and now?"<<endl;
-        cout<<vec->size()<<endl;
-        sizeIncrement();
-//        int i=vec->size();
-//        size=&i;
-//        cout<<*size;
 
+    void Game::addPlayer(Player& name){
+//        cout<<playerNamesP.size()<<"sizzzzz"<<endl;
+//        for (Player* p : this->playerNamesP) {
+//            cout<<p->getName()<<endl;
+//        }
+        if(playerNamesP.size()==6){
 
-//        vector<Player>* v =this->playerNamesP*;
-//        for (uint i = 0; i < this->playerNamesP->size(); i++) {
-//            cout<<this->playerNamesP->at(i)->getName()<<endl;
-//            cout<<this->playerNamesP->size()<<endl;
+            throw invalid_argument("too many");
+        }
+//        vector<Player*>* vec=myVector();
+
+        playerNamesP.push_back(&name);
+
+//        cout << "ater" << endl;
+//        for (Player* p : this->playerNamesP) {
+//            cout<<p->getName()<<endl;
 //        }
 
-//            name.index=this->playerNames.size();
-//        this->playerNames.push_back(name);
+//        size++;
 
-//        vector<string*> vec=coup::Game::getPlayers();
-//        this->playerNames.push_back(name);
-//        vector<string> vec=getPlayers();
-//        vec.push_back(&name);
-//        this->playerNamesP.push_back(&name);
-//        cout<<"num\n"+name;
-    }
-    void Game::sizeIncrement(){
-        this->size++;
-    }
-
-    bool Game::playerExist(Player &name) {
-
-        vector<Player *> *vec = myVector();
-        for (uint i = 0; i < vec->size(); i++) {
-            if (vec->at(i)->_name == name.getName()) {
-                return true;
-            }
-        }
-        return false;
     }
 
     void Game::removePlayer(Player &name) {
-////        vector<Player*> v = this->playerNamesP;
-        vector<Player*>* vec=myVector();
 
-        for (uint i = 0; i < vec->size(); i++) {
-            if (vec->at(i)->_name == name.getName()) {
-                vec->erase(vec->begin() + i);
-                size--;
-                return;
+            for (Player* p : this->playerNamesP) {
+                if (p->_name == name._name){
+                    if (!p->alive){
+                        throw ("is dead already");
+                    }
+                    p->alive = false;
+                    return;
+                }
             }
-
-        }
-        throw("no exist");
-
+            throw("no exist");
     }
-//        this->playerNames.erase(this->playerNames.begin()+name.index);
-//        this->playerNames.remove(this->playerNames.begin(),this->playerNames.end(),name);
-//        this->playerNames.erase(std::remove(this->playerNames.begin(),this->playerNames.end(),name),this->playerNames.end());
-
 
 
     string Game::turn() {
-        if(size>0) {
-            vector<Player *> *vec = this->myVector();
-            cout << this->size << " " << this->GameTurn << "  " << vec->at((uint) this->GameTurn % (uint) size)->_name
-                 << endl;
-            return this->myVector()->at((uint) this->GameTurn % (uint) size)->_name;
+        if(playerNamesP.size()>0) {
+            return this->playerNamesP.at(currTurn)->getName();
         }
         else
             return "x";
-//        return players().at(1);//(uint)this->GameTurn%this->myVector()->size());
     }
-    void Game::turnIncrement() {
-        GameTurn++;
-    }
+
 
     string Game::winner() {
         if(started==false){
             throw("isnt started");
         }
-        if(size==1){
+        if(players().size()==1){
                 return players().at(0);
         }
         else{
             throw("Game isn't over");
         }
-        return "";
     }
 
+    void Game::nextTurn() {
+        for (uint i = this->currTurn + 1; i < this->playerNamesP.size() + this->currTurn; ++i) {
+            if (this->playerNamesP.at(i % this->playerNamesP.size())->alive) {
+//                cout<<"this currTurn="<<currTurn<<endl;
+                this->currTurn = i % this->playerNamesP.size();
+//                cout<<"next currTurn="<<i % this->playerNamesP.size()<<"  "<<i<<playerNamesP.size()<<endl;
+                break;
+            }
+        }
+    }
 }
