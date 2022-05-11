cf41c22bf705289089062cceadee53218b2fc8b9
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 19:35:31 +0300

    Update Captain.hpp

--- sources/Captain.hpp
+++ sources/Captain.hpp
@@ -1,30 +1,21 @@
 #pragma once
 
 #include "Player.hpp"
-//#include "Game.hpp"
 
-//#ifndef GAME_HPP
-//#define GAME_HPP
-//#include "Captain.hpp"
-//#endif
 namespace coup {
     class Player;
-//    class Player;
-
 
     class Captain : public coup::Player {
     public:
-//        Game game;
-//        string _name;
+
     string _role;
+
         Captain(Game &g,  string n);
 
         void steal( Player &p);
 
-        void block(const Player &p);
+            void block( Player &p);
 
         string role();
-
-
     };
 }
