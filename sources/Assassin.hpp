9ffe80abb22141e761fbc6f3862a1d65ea05cbac
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 19:34:44 +0300

    Update Assassin.hpp

--- sources/Assassin.hpp
+++ sources/Assassin.hpp
@@ -9,18 +9,12 @@
 //#endif
 namespace coup {
     class Player;
-//    class Player;
-
     class Assassin : public coup::Player {
     private:
-//        Game game;
         string _role;
     public:
         Assassin(Game &g,  string n);
         string role();
         void coup( Player &p);
-
-
-
     };
 }
