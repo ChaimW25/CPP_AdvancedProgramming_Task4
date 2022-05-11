3c569db1ad88def22536262e0439680130ad1931
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 11:48:33 +0300

    Update Contessa.cpp

--- sources/Contessa.cpp
+++ sources/Contessa.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
-#include "Player.hpp"
+//#include "Player.hpp"
 #include "Contessa.hpp"
 #include <cstring>
 using namespace std;
 
 namespace coup {
 
-    Contessa::Contessa(Game g, string n) {
-    this->game = move(g);
-    this->_name = n;
+    Contessa::Contessa(Game &g, string name): Player(g,name) {
+//    this->game = move(g);
+//    this->_name = n;
     this->_role="Contessa";
-    this->coinNum=0;
-    g.addPlayer(*this);
+//    this->coinNum=0;
 
     }
 
 
 
     void Contessa::block(const Player &p){
-    }
+        this->game.turnIncrement();
 
+    }
+    string Contessa::role(){
+        return "Contessa";
+    }
 }
+
+
 
