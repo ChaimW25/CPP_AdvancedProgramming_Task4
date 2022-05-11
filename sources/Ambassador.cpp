1ccf827ef8b9a20c7f409c7421be172731fa9cdc
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 11:46:20 +0300

    Update Ambassador.cpp

--- sources/Ambassador.cpp
+++ sources/Ambassador.cpp
@@ -6,22 +6,39 @@ using namespace std;
 
 namespace coup {
 
-    Ambassador::Ambassador(Game g,  string name) {
-        this->game = std::move(g);
-        this->_name = name;
+    Ambassador::Ambassador(Game &g,  string name): Player(g,name) {
+//        this->game = std::move(g);
+//        this->_name = name;
         this->_role="Ambassador";
-        this->coinNum=0;
-        g.addPlayer(*this);
-        cout<<"hiii";
+//        this->coinNum=0;
+//        this->game.size++;
+//        cout<<"hiii";
     }
 
     void Ambassador::block(const Player &p){
+        this->game.turnIncrement();
+
     }
 
-    void Ambassador::transfer(const Player &p1, const Player &p2){
+    void Ambassador::transfer( Player &p1,  Player &p2){
+        if(p1.coinNum>=1) {
+            p1.coinNum -= 1;
+            p2.coinNum += 1;
+            this->game.turnIncrement();
+            cout<<"nows turn??"<<this->game.turn()<<endl;
+        }
+        else{
+            throw("not enough money");
+        }
+
     }
     void Ambassador::unsteal(){
+        this->game.turnIncrement();
 
+
+    }
+    string Ambassador::role(){
+        return "Ambassador";
     }
 
 }
