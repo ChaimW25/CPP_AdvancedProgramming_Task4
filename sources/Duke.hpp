a1fafd8d7f5220ec0728b42ff7bbfaf8cba6be92
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 11:50:07 +0300

    Update Duke.hpp

--- sources/Duke.hpp
+++ sources/Duke.hpp
@@ -1,32 +1,31 @@
-#include <iostream>
-//#include "Player.hpp"
-#include "Duke.hpp"
-#include <cstring>
-using namespace std;
+#pragma once
 
+#include "Player.hpp"
+//#include "Game.hpp"
+
+//#ifndef GAME_HPP
+//#define GAME_HPP
+//#include "Duke.hpp"
+//#endif
 namespace coup {
+//    class Game;
+    class Player;
+//    class Player;
 
-    Duke::Duke(Game &g,  string name) : Player(g,name){
-//        this->game = move(g);
-//        this->_name = n;
-        this->_role="Duke";
-//        this->coinNum=0;
-    }
-    void Duke::block(const Player &p) {
-        this->game.turnIncrement();
+    class Duke : public Player {
+    private:
+//        Game game;
+//        string _name;
+        string _role;
+    public:
+        Duke(Game &g,  string n);
 
-    }
-    void Duke::tax() {
-        this->coinNum+=3;
-        cout<<"the turn is"<<this->getName()<<endl;
-        this->game.turnIncrement();
-        cout<<"nows turn??"<<this->game.turn()<<endl;
+        void block(const Player &p);
+
+        void tax();
+
+        string role();
 
 
-    }
-    string Duke::role() {
-        return "Duke";
-    }
-
+    };
 }
-
