edb6f98e698ec5622293e097a7a0a455c76b0e2d
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 19:38:06 +0300

    Update Game.hpp

--- sources/Game.hpp
+++ sources/Game.hpp
@@ -3,37 +3,18 @@
 #include <string>
 #include <iostream>
 #include <vector>
-//#include "Player.hpp"
-//#include "Ambassador.hpp"
-//#include "Assassin.hpp"
-//#include "Captain.hpp"
-//#include "Contessa.hpp"
-//#include "Duke.hpp"
-
-//#ifndef AME_HPP
-//#define AME_HPP
-//#endif
-
-//#ifndef GAME_HPP
-//#define GAME_HPP
-//#include "Game.hpp"
-//#endif
 
 using namespace std;
 namespace coup {
     class Player;
-//    class Ambassador;
-//    class Assassin;
-//    class Duke;
-//    class Contessa;
-//    class Captain;
     class Game {
     public:
 
 //        vector<Player> playerNames;
-        int GameTurn;
-        int size=0;
+//        int GameTurn;
+//        int size=0;
         bool started=false;
+        size_t currTurn=0;
         vector<Player*> playerNamesP;
 
 
@@ -41,25 +22,26 @@ namespace coup {
 
         Game();
 
-        bool playerExist(Player &p);
+//        bool playerExist(Player &p);
 
-        void sizeIncrement();
+//        void sizeIncrement();
 
 
             vector<string> players();
 
-        vector<Player*>* myVector();
+//        vector<Player*>* myVector();
 
 //        vector<Player> getPlayers();
 
-        void addPlayer(Player *name);
+        void addPlayer(Player& name);
 
         void removePlayer(Player &name);
 
 
          string turn();
 
-         void turnIncrement();
+//         void turnIncrement();
+        void nextTurn();
 
          string winner();
 
