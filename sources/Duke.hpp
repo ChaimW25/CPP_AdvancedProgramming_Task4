0d592751d5bd8b016c5864afb758eb619cf765be
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2022-05-11 19:37:19 +0300

    Update Duke.hpp

--- sources/Duke.hpp
+++ sources/Duke.hpp
@@ -1,26 +1,18 @@
 #pragma once
 
 #include "Player.hpp"
-//#include "Game.hpp"
 
-//#ifndef GAME_HPP
-//#define GAME_HPP
-//#include "Duke.hpp"
-//#endif
 namespace coup {
-//    class Game;
     class Player;
-//    class Player;
 
     class Duke : public Player {
-    private:
-//        Game game;
-//        string _name;
-        string _role;
     public:
-        Duke(Game &g,  string n);
 
-        void block(const Player &p);
+        string _role;
+
+        Duke(Game &g, string n);
+
+        void block( Player &p);
 
         void tax();
 
